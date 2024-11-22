ab71389fddd2f07e5eddba3d579a6697f7a0f73d
PerfekTy <sebastianfelenczak@gmail.com>
2024-11-22 11:07:27 +0100

    Refactor boss movement logic to include speed variable for smoother animation

--- src/classes/game/canvas/boss.js
+++ src/classes/game/canvas/boss.js
@@ -1,30 +1,25 @@
 const bossGif = GIF();
 let bossX = 0;
 let direction = 2;
+const bossSpeed = 0.5;
 bossGif.load("./src/assets/images/boss.gif");
 bossGif.onload = function () {
-    bossLoadComplete();
-}
+  bossLoadComplete();
+};
 
 function bossSystem() {
-    generateBoss();
-    bossX += direction;
-    bossChangeDirection();
+  generateBoss();
+  bossX += direction * bossSpeed;
+  bossChangeDirection();
 }
 
 function generateBoss() {
-    ctx.drawImage(
-        bossGif.image,
-        bossX,
-        canvas.height / 2 - 330,
-        150,
-        150
-    );
-    bossX += direction;
+  ctx.drawImage(bossGif.image, bossX, canvas.height / 2 - 330, 150, 150);
+  bossX += direction * bossSpeed;
 }
 
 function bossChangeDirection() {
-    if (bossX > canvas.width - 150 || bossX < 0) {
-        direction = -direction;
-    }
+  if (bossX > canvas.width - 150 || bossX < 0) {
+    direction = -direction;
+  }
 }
