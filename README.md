be8b2b836180d14fde1f829e3d55fab23c5cad75
Sebastian <sebastianfelenczak@gmail.com>
2024-11-25 12:29:02 +0100

    Update README and HTML title for Bullet Hell Typing Game; enhance project description and features

--- README.md
+++ README.md
@@ -1,11 +1,90 @@
-## ZAŁOŻENIA GŁÓWNE
+# Bullet Hell Typing Game
 
-1. Gra typu Textorcist - bullet hell [2D].
-2. Kilka poziomów lub jeden endless (jest coraz trudniej z czasem).
-3. Boss, który strzela w gracza on musi unikać i przy okazji wpisywać różne kwestie na klawiaturze, aby zadawać obrażenia przeciwnikowi.
-4. System kolizji pocisków oraz samych przeciwników. 
-5. Rożne rodziaje pocisków przeciwnika tj. rozbijanie na mniejsze, różny pathing (być może losowy).
-6. Grafika typu pixel-art.
-7. Gra w postaci aplikacji webowej.
-8. Dokumentacja najważniejszych modułów i całej aplikacji.
+## Opis Projektu
 
+"Bullet Hell Typing Game" to dynamiczna gra 2D typu bullet hell inspirowana grą [Textorcist](https://store.steampowered.com/app/940680/The_Textorcist_The_Story_of_Ray_Bibbia/). Łączy w sobie elementy zręcznościowe z wymagającym wpisywaniem tekstu, zapewniając jednocześnie intensywną rozgrywkę i unikalne wyzwania. Gracz wciela się w postać, która unika pocisków wroga i jednocześnie musi szybko wpisywać frazy, aby zadawać obrażenia bossowi. Gra cechuje się grafiką w stylu pixel-art i została zaprojektowana jako aplikacja webowa.
+
+## Główne Funkcje
+
+### 1. Mechanika Rozgrywki
+
+- Intensywna akcja w stylu bullet hell.
+
+- Wpisywanie tekstu na klawiaturze jako główne narzędzie ataku.
+
+- Tryb endless, w którym wyzwanie rośnie wraz z czasem.
+
+### 2. System Kolizji
+
+- Detekcja kolizji między pociskami przeciwników a graczem.
+
+- Możliwość interakcji z pociskami o różnych właściwościach.
+
+### 3. Bossowie i Pociski
+
+- Główny przeciwnik, który strzela różnymi rodzajami pocisków:
+
+- Pociski rozpraszające się na mniejsze części.
+
+- Pociski z różnym lub losowym patternem ruchu.
+
+### 4. Grafika i Styl
+
+- Artystyczny styl pixel-art, nadający grze nostalgiczny i estetyczny charakter.
+
+### 5. Technologia
+
+- Aplikacja webowa dostępna w przeglądarce.
+
+- Wydajny system renderowania dla płynnej i responsywnej rozgrywki.
+
+## Cele Projektu
+
+- Stworzenie angażującej i wyzywającej gry zręcznościowej.
+
+- Zapewnienie płynnego działania za pomocą technologii webowych.
+
+- Dokumentacja wszystkich kluczowych modułów i struktury aplikacji, aby ułatwić dalszy rozwój i wsparcie projektu.
+
+## Technologie Użyte w Projekcie
+
+**Frontend:**
+
+- HTML
+- CSS
+- JavaScript
+
+**Silnik Gry:**
+
+- Canvas dla renderowania 2D
+
+**System Kolizji:**
+
+- Dedykowane algorytmy do wykrywania kolizji w czasie rzeczywistym.
+
+## Jak uruchomić projekt lokalnie
+
+1. Sklonuj repozytorium.
+
+```bash
+git clone https://github.com/PerfekTy/bullet-hell-typing-game.git
+cd bullet-hell-typing-game
+```
+
+2. Jeżeli nie masz wtyczki [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) zainstaluj http-server.
+
+```bash
+npm install --global http-server
+```
+
+3. Uruchom server.
+
+```bash
+npx http-server .
+```
+
+4. Otwórz przeglądarkę i przejdź pod adres http://localhost:8080.
+
+## Jak uruchomić projekt online
+
+Wejdz w link -> [Bullet Hell Typing Game](perfekty.github.io/psam-game/)
--- src/controllers/startGame.js
+++ src/controllers/startGame.js
@@ -1,69 +1,71 @@
-$(document).ready(function() {
-    // Time of reappearing animation
-    const interval = 1000;
-    let textAnimation;
-    let gameRunning = false;
+$(document).ready(function () {
+  // Time of reappearing animation
+  const interval = 1000;
+  let textAnimation;
+  let gameRunning = false;
 
-    // Remove to see menu
-    gameRunning = skipMenu(gameRunning);
+  // Remove to see menu
+  // gameRunning = skipMenu(gameRunning);
 
-    textAnimation = animateText(interval, "menuStart");
+  textAnimation = animateText(interval, "menuStart");
 
-    // Check if the key was pressed
-    $(document).keydown(function() {
-        // Checks if the game is already running to prevent multiple game starts
-        if (!gameRunning) {
-            gameRunning = true;
-            startGame(function() {
-                hideMenuElements();
+  // Check if the key was pressed
+  $(document).keydown(function () {
+    // Checks if the game is already running to prevent multiple game starts
+    if (!gameRunning) {
+      gameRunning = true;
+      startGame(function () {
+        hideMenuElements();
 
-                // Loading the view of the game
-                $('#game').load('./src/view/game.html', function() {
-                    // Loading the logic of the game
-                    $.getScript("./src/controllers/game.js");
-                });
-            });
-        }
-    })
+        // Loading the view of the game
+        $("#game").load("./src/view/game.html", function () {
+          // Loading the logic of the game
+          $.getScript("./src/controllers/game.js");
+        });
+      });
+    }
+  });
 });
 
 // Animation of appearing and disappearing text
 function animateText(interval, textId) {
-    return setInterval(function() {
-        $("#" + textId).animate({opacity: 0}, interval).animate({opacity: 1}, interval);
-    }, interval * 2);
+  return setInterval(function () {
+    $("#" + textId)
+      .animate({ opacity: 0 }, interval)
+      .animate({ opacity: 1 }, interval);
+  }, interval * 2);
 }
 
 function playSound(soundFile) {
-    let audio = new Audio(soundFile);
-    audio.play();
+  let audio = new Audio(soundFile);
+  audio.play();
 }
 
 // Animation of disappearing menu and playing the sound effect
 function startGame(callback) {
-    playSound("./src/sound/startGame.wav");
-    $("#menuStart").stop(true).css('opacity', 0);
-    $("#menuStart").animate({fontSize: 0}, 1800);
-    $("#menuTitle").animate({fontSize: 0}, 1800);
-    $("#bg-video").animate({opacity: 0}, 1800, function() {
-        callback();
-    });
+  playSound("./src/sound/startGame.wav");
+  $("#menuStart").stop(true).css("opacity", 0);
+  $("#menuStart").animate({ fontSize: 0 }, 1800);
+  $("#menuTitle").animate({ fontSize: 0 }, 1800);
+  $("#bg-video").animate({ opacity: 0 }, 1800, function () {
+    callback();
+  });
 }
 
 function hideMenuElements() {
-    $("#menu").hide();
+  $("#menu").hide();
 }
 
 // Skips menu screen (dev purpose)
 function skipMenu(gameRunning) {
-    gameRunning = true;
-    hideMenuElements();
-    $('#game').load('./src/view/game.html', function() {
-        $.getScript("./src/controllers/game.js");
-    });
-    // $('#gameOver').load('./src/view/gameOver.html', function() {
-    //     $.getScript("./src/controllers/gameOver.js");
-    // });
+  gameRunning = true;
+  hideMenuElements();
+  $("#game").load("./src/view/game.html", function () {
+    $.getScript("./src/controllers/game.js");
+  });
+  // $('#gameOver').load('./src/view/gameOver.html', function() {
+  //     $.getScript("./src/controllers/gameOver.js");
+  // });
 
-    return gameRunning;
+  return gameRunning;
 }
